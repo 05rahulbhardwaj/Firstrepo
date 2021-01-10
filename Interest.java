4811f2d23ae612f8f4850dfb07f7911f8b5e26eb
Rahul Bhardwaj <40406790+05rahulbhardwaj@users.noreply.github.com>
2021-01-10 18:34:22 +0530

    Add files via upload

---
+++ Interest.java
@@ -0,0 +1,11 @@
+public class Interest
+{
+public static void main(int p,int r,int t)
+{
+double si,amt;
+si=(p*r*t)/100;
+amt=si+p;
+System.out.println("The Simple Interest="+si);
+System.out.println("The amount="+amt);
+}
+}
